b7d32759254b0035619f2c7437fcd48bb0277a39
BrunaDaloia <bruna-daloia@hotmail.com>
2020-10-13 13:24:53 -0300

    create_map

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+import landing from './pages/landing';
+import orphanagesMap from './pages/orphanagesMap';
 
 function Routes() {
   return (
-
+  <BrowserRouter>
+  <Switch>
+  <Route path="/" exact component={landing} />
+  <Route path="/app" component={orphanagesMap} />
+  </Switch>
+  </BrowserRouter>
   );
 }
 
