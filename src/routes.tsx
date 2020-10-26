1c5f9bb9f46f6a69675534bd7b14bec963aa03a9
BrunaDaloia <bruna-daloia@hotmail.com>
2020-10-25 23:11:14 -0300

    last_commit

--- src/routes.tsx
+++ src/routes.tsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
 import landing from './pages/landing';
-import orphanagesMap from './pages/orphanagesMap';
+import OrphanagesMap from './pages/OrphanagesMap';
+import Orphanage from './pages/Orphanage';
+import CreateOrphanage from './pages/CreateOrphanage';
 
 function Routes() {
   return (
   <BrowserRouter>
   <Switch>
   <Route path="/" exact component={landing} />
-  <Route path="/app" component={orphanagesMap} />
+  <Route path="/app" component={OrphanagesMap} />
+
+  <Route path="/orphanages/create" component={CreateOrphanage} />
+  <Route path="/orphanages/:id" component={Orphanage} />
   </Switch>
   </BrowserRouter>
   );
